4be75ced8cbff9a685046effdd134c3babe2a14a
nutzchannel <spencie01@gmail.com>
2015-11-18 22:17:51 -0500

    Rough basis
    I'm going to bed

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""
+INITIALIZER is a Python module created by Spencer Baumann
+to help detect save files, and create them, all using the
+amazing JSON module. All code is compliant with the Google
+Style Guide for Python, especially in that all name follows
+the correct capitalization.
+"""
---
+++ check_file.py
@@ -0,0 +1,17 @@
+import os.path
+def __init__(file_dir):
+    """
+    INITIALIZER.CHECK_FILE
+    This is a simple function that checks if
+    the file specified, usually a save file,
+    is existent, which is a semi-reliable way
+    to know if this is the first run.
+    INPUTS:
+        String file_dir: The relative location of the save file.
+    OUTPUT:
+        Boolean: If true, the savefile doesn't exist, and is free to write.
+        If false, the savefile already exists.
+    """
+    file = open(file_dir, "r")
+
+    file.close()
