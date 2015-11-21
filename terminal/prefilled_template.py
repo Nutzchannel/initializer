cd2cef8029416d8592d6b50eb6fc7cd33f96733d
nutzchannel <spencie01@gmail.com>
2015-11-21 00:58:24 -0500

    Docs, Docs, Docs

---
+++ terminal/prefilled_template.py
@@ -0,0 +1,8 @@
+def __init__:
+    """
+    INITIALIZER.TERMINAL.PREFILLED_TEMPLATE
+    This is a template function allowing the developer
+    to simply enter the application name, as well as
+    some other data, in order to create a quick, fast
+    init function straight out of SBLUH.py
+    """
